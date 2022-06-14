4f655b7f7033facaed2e10fee98b5c20361d4d33
Onikoyi Ayomide <ayomideonikoyi@gmail.com>
2022-06-14 19:56:08 +0100

    DevNet Dy1

---
+++ first_project.js
@@ -0,0 +1,21 @@
+// Operators
+let first_num;
+let second_num;
+let total_num;
+
+// let first_num, second_num, total_num
+// let first_num
+first_num = 50
+second_num = 6544
+
+// total_num = first_num + second_num
+// total_num = first_num - second_num
+// total_num = first_num * second_num
+// total_num = first_num / second_num
+// total_num = (56 + 5) * 56 + 0.6654
+// total_num= 50 * "three"
+let no = 45
+
+console.log(no.toString())
+
+
